f766c7635d75f9836ee4a123c8993d0eb5ac715d
enebin <enebin777@gmail.com>
2024-07-14 22:10:01 +0900

    [feat][#1] 임시 root vc 추가

---
+++ Targets/Mashow/Sources/MashowRootViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MashowRootViewController.swift
+//  MashowKit
+//
+//  Created by Kai Lee on 7/14/24.
+//  Copyright © 2024 io.alcoholers. All rights reserved.
+//
+
+import UIKit
+
+class MashowRootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
