3bb43e78a44108e71f6847e9e0cc872a9ea9205f
enebin <enebin777@gmail.com>
2024-07-21 20:28:05 +0900

    [chore][#3] Moya 참조 버전 변경 - master를 보게 하면 update가 안 됨

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -14,7 +14,7 @@ let dependencies = Dependencies(
         // Moya
         /// Note: Moya가 22년 이후로 관리가 안 되고 있어서 min iOS version이 아직도 v10으로 되어 있음
         /// 때문에 Moya를 포크해서 버전을 iOS 12로 올린 레포를 대신 사용함(코드 전부 동일한데 버전만 다름)
-        .remote(url: "https://github.com/enebin/Moya-iOS12.git", requirement: .branch("master")),
+            .remote(url: "https://github.com/enebin/Moya-iOS12.git", requirement: .upToNextMinor(from: "1.0.0")),
         // Kingfisher
         .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0")),
         // Lottie
