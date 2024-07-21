592e9efab377ca3ba61315be4134e0fc4ee4fca5
enebin <enebin777@gmail.com>
2024-07-21 21:20:09 +0900

    [test][#3] 네트워크 레이어 테스트 추가

---
+++ Targets/Mashow/Tests/Util/NetworkMangerTests.swift
@@ -0,0 +1,120 @@
+//
+//  NetworkMangerTests.swift
+//  MashowTests
+//
+//  Created by Kai Lee on 7/21/24.
+//  Copyright © 2024 com.alcoholers. All rights reserved.
+//
+
+import XCTest
+import Moya
+@testable import Mashow
+import SwiftUI
+
+final class NetworkManagerTests: XCTestCase {
+    var networkManager: NetworkManager<TestAPI>!
+    
+    override func setUpWithError() throws {
+        let stubbingProvider = MoyaProvider<TestAPI>(stubClosure: MoyaProvider.immediatelyStub)
+        networkManager = NetworkManager(provider: stubbingProvider)
+    }
+
+    override func tearDownWithError() throws {
+        networkManager = nil
+    }
+    
+    func testRegisterAccessToken() {
+        let token = "testAccessToken"
+        
+        networkManager.registerAccessToken(token: token)
+        XCTAssertEqual(networkManager.accessToken, token, "Access token should be correctly registered.")
+    }
+    
+    func testRemoveAccessToken() {
+        networkManager.registerAccessToken(token: "testAccessToken")
+        networkManager.removeAccessToken()
+        
+        XCTAssertNil(networkManager.accessToken, "Access token should be removed.")
+    }
+    
+    func testRequestSuccess() async throws {
+        let expectedResult = testData
+        let apiEndpoint = TestAPI.testGet
+        
+        do {
+            let result = try await networkManager.request(apiEndpoint, of: TestData.self)
+            XCTAssertEqual(result, expectedResult, "The result should match the expected result.")
+        } catch {
+            XCTFail("Request should succeed, but it failed with error: \(error).")
+        }
+    }
+    
+    func testRequestFailure() async {
+        let stubbingProvider = MoyaProvider<TestAPI>(
+            endpointClosure: failureEndpointClosure,
+            stubClosure: MoyaProvider.immediatelyStub)
+        networkManager = NetworkManager(provider: stubbingProvider)
+        
+        let apiEndpoint = TestAPI.testGet
+
+        do {
+            let _: TestData = try await networkManager.request(apiEndpoint, of: TestData.self)
+            XCTFail("Request should fail, but it succeeded.")
+        } catch {
+            // Expect error
+        }
+    }
+    
+    // Helper to simulate request failure
+    private func failureEndpointClosure(_ target: TestAPI) -> Endpoint {
+        return Endpoint(
+            url: URL(target: target).absoluteString,
+            sampleResponseClosure: { .networkError(NSError(domain: "Network error", code: 500, userInfo: nil)) },
+            method: target.method,
+            task: target.task,
+            httpHeaderFields: target.headers
+        )
+    }
+}
+
+// MARK: - Test API
+extension NetworkManagerTests {
+    enum TestAPI: TargetType {
+        case testGet
+        
+        var method: Moya.Method {
+            .get
+        }
+        
+        var task: Task {
+            .requestPlain
+        }
+        
+        var headers: [String: String]? {
+            ["Content-Type": "application/json"]
+        }
+        
+        var path: String { "/get" }
+        
+        var sampleData: Data {
+            jsonString.data(using: .utf8)!
+        }
+        
+        var baseURL: URL {
+            URL(string: "https://api.example.com")!
+        }
+    }
+}
+
+// MARK: - Test data
+fileprivate struct TestData: Decodable, Equatable {
+    let data: String
+}
+
+fileprivate let jsonString = """
+{
+  "data": "testGet"
+}
+"""
+
+fileprivate let testData = TestData(data: "testGet")
