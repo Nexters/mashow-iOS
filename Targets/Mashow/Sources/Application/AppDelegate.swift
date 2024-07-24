9dfd27b2ff9883e7f721087017f16f9df819ef53
enebin <enebin777@gmail.com>
2024-07-24 22:34:58 +0900

    [chore][#7]  Kakao SDK 초기화 로직 추가

--- Targets/Mashow/Sources/Application/AppDelegate.swift
+++ Targets/Mashow/Sources/Application/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 
-import Kingfisher
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,7 +9,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
-
+        if
+            let kakaoAppKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as? String,
+            let baseUrl = Bundle.main.object(forInfoDictionaryKey: "BASE_API_URL") as? String
+        {
+            KakaoSDK.initSDK(appKey: kakaoAppKey)
+        }
+        
         return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (AuthApi.isKakaoTalkLoginUrl(url)) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+
+        return false
     }
 }
