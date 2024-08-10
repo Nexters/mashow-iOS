6bb2a73e06988757f5fab136972256187eb579d7
enebin <enebin777@gmail.com>
2024-08-11 07:56:31 +0900

    [feat][#26] 텍스트필드 방어 로직 추가

--- Targets/Mashow/Sources/FoodInput/FoodInputViewController.swift
+++ Targets/Mashow/Sources/FoodInput/FoodInputViewController.swift
@@ -20,7 +20,7 @@ class FoodInputViewController: UIViewController {
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "먹은 음식 이름 또는 종류를 적어주세요!"
-        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        label.font = .pretendard(size: 20, weight: .semibold)
         label.textColor = .white
         label.textAlignment = .center
         return label
@@ -47,8 +47,8 @@ class FoodInputViewController: UIViewController {
         
         let addButton = UIButton(type: .system)
         addButton.setTitle("+ 추가하기", for: .normal)
-        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .regular)
-        addButton.setTitleColor(.white, for: .normal)
+        addButton.titleLabel?.font = .pretendard(size: 14, weight: .regular)
+        addButton.setTitleColor(.white.withAlphaComponent(0.7), for: .normal)
         addButton.addTarget(self, action: #selector(didTapAddButton), for: .touchUpInside)
         
         footerView.addSubview(addButton)
@@ -63,7 +63,7 @@ class FoodInputViewController: UIViewController {
     lazy var doneButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("완료", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.titleLabel?.font = .pretendard(size: 20, weight: .medium)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .systemBlue
         button.layer.cornerRadius = 8
@@ -111,6 +111,21 @@ private extension FoodInputViewController {
     func setupTableFooter() {
         addFooterView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 60)
         tableView.tableFooterView = addFooterView
+        updateFooterVisibility() // Initial check
+        updateDoneButtonEnability() // Initial check
+    }
+    
+    func updateFooterVisibility() {
+        if let lastItem = foodItems.last, lastItem.isEmpty {
+            tableView.tableFooterView?.isHidden = true // Hide footer
+        } else {
+            tableView.tableFooterView?.isHidden = false // Show footer
+        }
+    }
+    
+    func updateDoneButtonEnability() {
+        doneButton.isEnabled = isResultSubmittable(result: foodItems)
+        doneButton.alpha = doneButton.isEnabled ? 1.0 : 0.5
     }
 }
 
@@ -119,10 +134,13 @@ private extension FoodInputViewController {
     @objc func didTapAddButton() {
         foodItems.append("")
         tableView.reloadData()
+        updateFooterVisibility()
     }
     
     @objc func didTapDoneButton() {
-        // Handle completion logic, such as saving the entered food items.
+        let chosenFoods = foodItems.filter { !$0.isEmpty }
+        // FIXME: fixme
+        print(chosenFoods)
     }
 }
 
@@ -143,6 +161,8 @@ extension FoodInputViewController: UITableViewDataSource {
     
     @objc func textFieldDidChange(_ textField: UITextField) {
         foodItems[textField.tag] = textField.text ?? ""
+        updateFooterVisibility()
+        updateDoneButtonEnability()
     }
 }
 
@@ -150,6 +170,18 @@ extension FoodInputViewController: UITableViewDataSource {
 extension FoodInputViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
+    }
+}
+
+// MARK: - Utils
+private extension FoodInputViewController {
+    func isResultSubmittable(result: [String]) -> Bool {
+        let chosenFoods = foodItems.filter { !$0.isEmpty }
+        guard !chosenFoods.isEmpty else {
+            return false
+        }
+                
+        return true
     }
 }
 
