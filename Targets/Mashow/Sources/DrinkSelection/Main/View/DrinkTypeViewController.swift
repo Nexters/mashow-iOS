7b26faefe0e71676fcadfab73b056722f5a70886
enebin <enebin777@gmail.com>
2024-09-23 20:15:04 +0900

    [fix][#57] 주종 선택 UX 개선

--- Targets/Mashow/Sources/DrinkSelection/Main/View/DrinkTypeViewController.swift
+++ Targets/Mashow/Sources/DrinkSelection/Main/View/DrinkTypeViewController.swift
@@ -14,7 +14,7 @@ final class DrinkTypeViewController: UIViewController {
     
     let viewModel: DrinkSelectionViewModel
     var drinkType: DrinkType
-    var cancellables = Set<AnyCancellable>()
+    private var cancellables = Set<AnyCancellable>()
     
     init(viewModel: DrinkSelectionViewModel, drinkType: DrinkType) {
         self.viewModel = viewModel
@@ -110,6 +110,12 @@ final class DrinkTypeViewController: UIViewController {
         let image = UIImage(named: drinkType.rawValue + "Bottle")
         let imageView = UIImageView(image: image)
         imageView.contentMode = .scaleAspectFill
+        
+        // Trigger drink addition when image tapped
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(addDrinkType))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        
         return imageView
     }()
     
@@ -118,7 +124,7 @@ final class DrinkTypeViewController: UIViewController {
         bind()
         setupLayouts()
         setupHandlers()
-        viewModel.state.currentType.send(drinkType) // FIXME: - 드래그 조금만 해도 호출돼서 버그있음
+        viewModel.state.currentType.send(drinkType)
     }
 }
 
